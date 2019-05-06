8495075e96a1afbc531e641b2aeced3da501797d
alena <a30058422@gmail.com>
2019-05-06 22:33:14 +0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { OverlayModule } from '@angular/cdk/overlay';
+
 
 import { AppComponent } from './app.component';
+import { FilterPipe } from './filter.pipe';
+import { AutocompleteModule } from './autocomplete/autocomplete.module';
+import { AutocompleteComponent } from './autocomplete/autocomplete.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FilterPipe
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    OverlayModule,
+    AutocompleteModule
   ],
   providers: [],
+  entryComponents:[AutocompleteComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
